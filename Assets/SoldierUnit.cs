817ed773402d9d2d8098238c20961375a4789bb5
Joëlle Perritaz <joelle.perritaz@gmail.com>
2018-11-10 18:20:48 +0100

    La base est là !

---
+++ Assets/SoldierUnit.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierUnit : Unit {
+
+    public override void MarkAsAttacking(Unit other)
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+
+    public override void MarkAsDefending(Unit other)
+    {
+        GetComponent<SpriteRenderer>().color = Color.yellow;
+    }
+
+    public override void MarkAsDestroyed()
+    {
+        GetComponent<SpriteRenderer>().color = Color.black;
+    }
+
+    public override void MarkAsFinished()
+    {
+        GetComponent<SpriteRenderer>().color = Color.gray;
+    }
+
+    public override void MarkAsFriendly()
+    {
+       // GetComponent<SpriteRenderer>().color = Color.green;
+    }
+
+    public override void MarkAsReachableEnemy()
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+
+    public override void MarkAsSelected()
+    {
+        GetComponent<SpriteRenderer>().color = Color.green;
+    }
+
+    public override void UnMark()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/TileGrassASquare.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class TileGrassASquare : Square {
+
+    public override Vector3 GetCellDimensions()
+    {
+        return GetComponent<SpriteRenderer>().bounds.size;
+    }
+
+    public override void MarkAsHighlighted()
+    {
+        GetComponent<SpriteRenderer>().color = Color.gray;
+    }
+
+    public override void MarkAsPath()
+    {
+        GetComponent<SpriteRenderer>().color = Color.blue;
+    }
+
+    public override void MarkAsReachable()
+    {
+        GetComponent<SpriteRenderer>().color = Color.yellow;
+    }
+
+    public override void UnMark()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
